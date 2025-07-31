6b7bdc001fb3f258c3e429c5e41ea83fc245dea8
jagruti <jagrutigoswami402@gmail.com>
2025-07-31 12:22:52 +0530

    add main.java

---
+++ main.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class main {
+    public static void main(String args[]){
+        System.out.println("this is java program");
+        System.out.println("output");
+        System.out.println(new Date());
+
+    }
+}
